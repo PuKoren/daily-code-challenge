708685a4b178af2e8ddf9c2b82682ee3837a9f25
PuKoren <koren@pukogames.com>
2015-03-06 21:15:57 +0100

    Add npm test command and fix jshint issues in index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('views', './views') 
-app.set('view engine', 'jade')
+app.set('views', './views');
+app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'})
-})
- 
-app.listen(3000)
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Hey',
+    message: 'Hello there!'
+  });
+});
+
+app.listen(3000);
