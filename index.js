2e553873466d5451ce2c2f6013dc6ecedc1b3f90
PuKoren <koren@pukogames.com>
2015-03-07 17:04:56 +0100

    Express now serves static content + compress data to gzip

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+  compression = require('compression'),
+  app = express();
+
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -10,5 +12,8 @@ app.get('/', function(req, res) {
     message: 'Hello there!'
   });
 });
+
+app.use(compression());
+app.use(express.static(__dirname + '/assets'));
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
