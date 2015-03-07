3b3d53bd0516dfbc47bde03a952cf45c1e7cfc21
PuKoren <koren@pukogames.com>
2015-03-07 16:29:07 +0100

    Add grunt task to jshint on save

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,31 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'index.js'],
+      options: {
+        reporter: require('jshint-stylish')
+      }
+    },
     nodemon: {
       dev: {
         script: 'index.js',
         options: {
           cwd: __dirname,
-          ignore: ['node_modules/**', 'public/**', 'src/**', 'views/**'],
+          ignore: ['node_modules/**', 'public/**', 'views/**'],
           ext: 'js',
-          watch: ['index.js'],
-          delay: 250,
-          legacyWatch: true
+          watch: ['index.js', 'src/**/*.js'],
+          delay: 250
         }
       }
     },
+    watch: {
+      files: ['index.js', 'src/**/*.js'],
+      tasks: ['jshint']
+    },
     concurrent: {
       dev: {
-        tasks: ['nodemon:dev'],
+        tasks: ['nodemon:dev', 'watch'],
         options: {
           logConcurrentOutput: true
         }
@@ -27,7 +36,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['concurrent:dev']);
-}
+};
