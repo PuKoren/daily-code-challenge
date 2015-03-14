aa3315696105af0238382b831796e7f3f4bc209a
PuKoren <koren@pukogames.com>
2015-03-14 21:30:17 +0100

    Add basic index page with fake title

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,9 @@
 module.exports = function(req, res) {
 	res.render('pages/index', {
-		title: 'Daily Code Challenge'
+		title: 'Daily Code Challenge',
+		challenge: {
+			title: 'Code a game where you can jump over incomming bricks, in the browser URI.',
+			repository: 'https://github.com/DailyCodeChallenge/challenge-1'
+		}
 	});
 };
