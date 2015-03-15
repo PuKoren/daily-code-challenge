8959df57644531926608a485a1a729a3785f518f
PuKoren <koren@pukogames.com>
2015-03-15 01:35:21 +0100

    Add github login URL

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'github-login-url': 'https://github.com/login/oauth/authorize',
+  'github-redirect-uri': 'http://localhost:3000/login/oauth/callback',
+  'github-client-id': '7936aa10be1396cf52cc',
+  'github-client-secret': '',
+  'github-scopes': ['user:email', 'user:follow', 'public_repo']
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,19 @@
+var config = require('../config');
+var uuid = require('node-uuid');
+
 module.exports = function(req, res) {
+	var loginUrl = 'https://github.com/login/oauth/authorize';
+	loginUrl += '?client_id=' + config['github-client-id'];
+	loginUrl += '&redirect_uri=' + config['github-redirect-uri'];
+	loginUrl += '&scope=' + config['github-scopes'].join(',');
+	loginUrl += '&state=' + uuid.v4();
+
 	res.render('pages/index', {
 		title: 'Daily Code Challenge',
 		challenge: {
 			title: 'Code a game where you can jump over incomming bricks, in the browser URI.',
 			repository: 'https://github.com/DailyCodeChallenge/challenge-1'
-		}
+		},
+		loginUrl: loginUrl
 	});
 };
